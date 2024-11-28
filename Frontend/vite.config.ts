20399043640cf91d1a398780763c8182ee954000
dikjain <dikshitmahanot2005@gmail.com>
2024-11-28 21:59:36 +0530

    done 2

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://printitout.onrender.com',
         changeOrigin: true,
       },
     },
