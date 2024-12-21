8bc15eb7a9e3bf0d0de559fcd7a20dd03992281e
dikjain <dikshitmahanot2005@gmail.com>
2024-12-21 13:46:53 +0530

    op

--- Frontend/src/pages/User/User.jsx
+++ Frontend/src/pages/User/User.jsx
@@ -52,12 +52,12 @@ export function UserDashboard() {
 
     setLoading(true);
     try {
-      const resultsResponse = await fetch('../Public/Assets/results.csv');
+      const resultsResponse = await fetch('"../../../Public/Assets/results.csv');
       const resultsCsvText = await resultsResponse.text();
       const parsedResultsData = Papa.parse(resultsCsvText, { header: false });
       const filteredResults = parsedResultsData.data.filter((row) => row.includes(enrolNo));
 
-      const subjectsResponse = await fetch('../Public/Assets/subjects.csv');
+      const subjectsResponse = await fetch('../../../Public/Assets/subjects.csv');
       const subjectsCsvText = await subjectsResponse.text();
       const parsedSubjectsData = Papa.parse(subjectsCsvText, { header: false });
       const subjectMap = new Map(parsedSubjectsData.data.map((row) => [row[0], row[2]]));
@@ -70,17 +70,17 @@ export function UserDashboard() {
 
       setResults(resultsWithPaperNames);
 
-      const studentsResponse = await fetch('../Public/Assets/students.csv');
+      const studentsResponse = await fetch('../../../Public/Assets/students.csv');
       const studentsCsvText = await studentsResponse.text();
       const parsedStudentsData = Papa.parse(studentsCsvText, { header: false });
       const filteredStudentData = parsedStudentsData.data.filter((row) => row.includes(enrolNo));
 
-      const institutesResponse = await fetch('../Public/Assets/institutes.csv');
+      const institutesResponse = await fetch('../../../Public/Assets/institutes.csv');
       const institutesCsvText = await institutesResponse.text();
       const parsedInstitutesData = Papa.parse(institutesCsvText, { header: false });
       const instituteMap = new Map(parsedInstitutesData.data.map((row) => [row[0], row[1]]));
 
-      const programmesResponse = await fetch('../Public/Assets/programmes.csv');
+      const programmesResponse = await fetch('../../../Public/Assets/programmes.csv');
       const programmesCsvText = await programmesResponse.text();
       const parsedProgrammesData = Papa.parse(programmesCsvText, { header: false });
       const programmeMap = new Map(parsedProgrammesData.data.map((row) => [row[0], row[1]]));
